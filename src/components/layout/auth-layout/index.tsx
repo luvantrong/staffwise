8a55ff1848e620b87dc3c7d6ea8302b34f37166e
tronglv <trongangel@gmail.com>
2025-11-22 23:26:18 +0700

    auth layout

--- src/components/layout/auth-layout/index.tsx
+++ src/components/layout/auth-layout/index.tsx
@@ -1,14 +1,16 @@
+import Text from '@components/core/text';
 import View from '@components/core/view';
 import colors from '@utils/constants/colors';
+import { height, width } from '@utils/helpers/dimension';
 import React, { Fragment } from 'react';
 import {
   ActivityIndicator,
+  Image,
+  Keyboard,
   StyleSheet,
   ViewProps,
-  Keyboard,
 } from 'react-native';
 import StatusBarLayout from '../status-bar';
-import Text from '@components/core/text';
 
 interface IProps extends ViewProps {
   title?: string;
@@ -47,7 +49,7 @@ const AuthLayout: React.FC<IProps> = ({
         statusBarColor={colors.primary.default}
       >
         <View
-          padding={20}
+          height={height * 0.35}
           style={styles.container}
           onStartShouldSetResponder={() => {
             Keyboard.dismiss();
@@ -55,14 +57,36 @@ const AuthLayout: React.FC<IProps> = ({
           }}
           {...props}
         >
-          {back}
-          <Text font="Inter" size={40} color={colors.white} weight={600}>
-            {title}
-          </Text>
+          <View style={styles.circleBackground}>
+            <Image source={require('@assets/images/circle-background.png')} />
+          </View>
+          <View style={[styles.circleLogo, styles.shadow]}>
+            <Image
+              style={styles.imgCircleLogo}
+              source={require('@assets/images/logo.png')}
+            />
+            <Text
+              color={colors.primary.default}
+              style={{
+                fontWeight: 'bold',
+              }}
+              size={20}
+            >
+              Staffwise
+            </Text>
+          </View>
+
+          <View padding={20}>
+            {back}
+            <Text font="Inter" size={40} color={colors.white} weight={600}>
+              {title}
+            </Text>
+          </View>
         </View>
         <View
           flex={1}
           padding={20}
+          paddingTop={width * 0.2}
           marginBottom={10}
           onStartShouldSetResponder={() => {
             Keyboard.dismiss();
@@ -82,5 +106,42 @@ const styles = StyleSheet.create({
   container: {
     flex: 0,
     backgroundColor: colors.primary.default,
+    borderBottomStartRadius: 48,
+    borderBottomEndRadius: 48,
+  },
+  circleBackground: {
+    position: 'absolute',
+    bottom: '-30%',
+    left: 0,
+  },
+  circleLogo: {
+    position: 'absolute',
+    bottom: -width * 0.2,
+    alignSelf: 'center',
+    zIndex: 10,
+    width: width * 0.4,
+    height: width * 0.4,
+    backgroundColor: colors.white,
+    borderRadius: width * 0.2,
+    flexDirection: 'column',
+    gap: 16,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  shadow: {
+    shadowColor: colors.black,
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.23,
+    shadowRadius: 2.62,
+    elevation: 4,
+  },
+  imgCircleLogo: {
+    width: width * 0.1,
+    height: width * 0.1,
+    backgroundColor: colors.primary.default,
+    borderRadius: width * 0.1,
   },
 });
